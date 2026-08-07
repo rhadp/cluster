80b3ada6114aa73153f6e2c2e9f134e566a4967d
Michael Kuehl <hello@micku.me>
2026-08-07 10:19:21 +0200

    ported the main roles to boostrap a RHAS cluster

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-source venv/bin/activate 
+source venv/bin/activate
+
+set -a
 source inventory/secrets/.env
+set +a
 
 INVENTORY_ARCH="arch-$CLUSTER_TOPOLOGY-$CLOUD_PROVIDER.yml"
 echo "INVENTORY_ARCH: $INVENTORY_ARCH"
 
-ansible-playbook -i "inventory/main.yml" -i "inventory/${INVENTORY_ARCH}" 1_bootstrap_cluster.yml
+ansible-playbook -i "inventory/main.yml" -i "inventory/platform.yml" -i "inventory/${INVENTORY_ARCH}" bootstrap_cluster.yml
